c46f08e4139e8441a3640907637b3ede35dfb0cd
Osawa Yoshiki <dasoku.leviathan449@gmail.com>
2017-10-30 14:30:58 +0900

    POJ3669

--- POJ3669.cpp
+++ POJ3669.cpp
@@ -1,90 +1,96 @@
-//Time Limit Exceeded
 #include <cstdio>
+#include <iostream>
 #include <queue>
+#include <vector>
 #include <algorithm>
 using namespace std;
-     
+
 int M;
-int meteor[302][302];
-int dist[302][302];
+int meteor[310][310];
+int dist[310][310];
 // int dx[5] = {0, 0, 0, -1, 1};
 // int dy[5] = {0, 1, -1, 0, 0};
 int dx[5] = {0, -1, 1, 0, 0};
 int dy[5] = {0, 0, 0, -1, 1};
+const int MAX = 302;
 const int INF = 1<<30;
-     
-inline bool inBoard(int x, int y)
-{
-  return (0 <= x && x <= 300 && 0 <= y && y <= 300);
+
+//座標(x, y)がBoardの範囲内ならばtrue
+inline bool inBoard(int x, int y){
+  return (0 <= x && x <= MAX && 0 <= y && y <= MAX);
 }
-     
-int main()
-{
-  int x, y, t;
-     
+
+int solve(){
+  //時刻0に(0, 0)に隕石が落下するとき
+  if(meteor[0][0] == 0) return -1;
+
+  //毎秒移動しなくてもよい場合
+  if (meteor[0][0] == INF){
+    //printf("0\n");
+    return 0;
+  }
+  
+  //int time = 0;
+  int cx = 0, cy = 0;
+  queue<pair<int,int> > que;
+  //ここから
+  que.push(make_pair(0, 0));
+  dist[0][0] = 0;
+  //ここ
+  while (!que.empty()){
+    pair<int,int> cur = que.front();
+    //que.pop();
+    cx = cur.first;
+    cy = cur.second;
+    que.pop();
+    if (meteor[cx][cy] == INF) {
+      return dist[cx][cy];
+    }
+    if (dist[cx][cy] < meteor[cx][cy]){
+      for (int j=1;j<5;j++){
+          int nx = cx + dx[j];
+          int ny = cy + dy[j];
+          int time = dist[cx][cy] + 1;
+          if (/*(inBoard(nx, ny)) &&*/
+              (dist[nx][ny] > time) && // まだ通っていない
+              (time < meteor[nx][ny])) // まだ通れる
+            {
+              // if (meteor[nx][ny] == INF){
+              //   //printf("%d\n", time);
+              //   return time;
+              // }
+              dist[nx][ny] = time;
+              que.push(make_pair(nx,ny));
+            }
+        }
+      }
+  }
+  return -1;
+}
+
+
+int main(){
+  int x, y, t;  
   scanf("%d", &M);
-     
-  for (int i=0;i<=302;i++)
-    for (int j=0;j<=302;j++)
-      {
+  
+  for (int i=0;i<MAX;i++){
+    for (int j=0;j<MAX;j++){
         meteor[i][j] = INF;
         dist[i][j] = INF;
       }
-     
+  }
+
   // 隕石のせいで入れなくなる時刻を記録
-  for (int i=0;i<M;i++)
-    {
-      scanf("%d%d%d", &x, &y, &t);
-      for (int j=0;j<5;j++)
-        if (inBoard(x+dx[j], y+dy[j]))
+  for (int i=0;i<M;i++){
+    scanf("%d%d%d", &x, &y, &t);
+      for (int j=0;j<5;j++){
+        if (inBoard(x+dx[j], y+dy[j])){
           meteor[x+dx[j]][y+dy[j]] = min(meteor[x+dx[j]][y+dy[j]], t);
-    }
-     
-  int time = 0;
-  int cx = 0, cy = 0;           // current place
-  queue<pair<int,int> > que;
-  if (meteor[0][0] == 0){
-      printf("-1\n");
-      return 0;
-    }  
-  if (meteor[0][0] == INF)
-    {
-      printf("0\n");
-      return 0;
-    }
-     
-  dist[0][0] = 0;
-  que.push(make_pair(0,0));
-     
-  while (!que.empty())
-    {
-      pair<int,int> cur = que.front();
-      que.pop();
-      cx = cur.first;
-      cy = cur.second;
-      if (dist[cx][cy] < meteor[cx][cy])
-        {
-          time = dist[cx][cy] + 1;
-          for (int j=1;j<5;j++)
-            {
-              int nx = cx + dx[j];
-              int ny = cy + dy[j];
-              if (inBoard(nx, ny) &&
-                  dist[nx][ny] == INF && // まだ通っていない
-                  time < meteor[nx][ny]) // まだ通れる
-                {
-                  if (meteor[nx][ny] == INF)
-                    {
-                      printf("%d\n", time);
-                      return 0;
-                    }
-                  que.push(make_pair(nx,ny));
-                  dist[nx][ny] = time;
-                }
-            }
         }
-    }
-  printf("-1\n");
+      }
+  }
+  cout << solve() << endl;
+  
   return 0;
 }
 
